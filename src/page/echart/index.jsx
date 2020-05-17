5627f4591a127329baa1ea30616cbba8336faab5
EquinoxFlowerFlow <xxzy1010@163.com>
2020-05-17 16:59:16 +0800

    'Echart随便弄了一个'

--- src/page/echart/index.jsx
+++ src/page/echart/index.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
+import echarts from 'echarts'
+import './styles.less'
 
 export default class Echart extends Component {
+  componentDidMount () {
+    var myChart = echarts.init(this.el);
+
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+          text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+          data:['销量']
+      },
+      xAxis: {
+          data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+          },
+          yAxis: {},
+          series: [{
+              name: '销量',
+              type: 'bar',
+              data: [5, 20, 36, 10, 10, 20]
+          }]
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+  }
   render() {
     return (
-      <div>
-        这是图表页面
+      <div className="Echart">
+        <div className="content" ref={v => this.el = v}>
+
+        </div>
       </div>
     )
   }
